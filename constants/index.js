421cc91366bd94d1409a69866e965aab4aaa6770
ABHIRUP <abhirupsaha02@gmail.com>
2023-08-13 14:58:50 +0530

    keypair added

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,7 @@
-import { PublicKey } from "@solana/web3.js"
+import * as solanaWeb3 from '@solana/web3.js'
 
-export const TODO_PROGRAM_PUBKEY = new PublicKey('')
+// connect to wallet
+let secretKey = Uint8Array.from([186, 51, 127, 107, 49, 133, 39, 198, 188, 6, 56, 99, 148, 61, 55, 105, 183, 174, 22, 148, 94, 130, 151, 26, 97, 242, 86, 11, 228, 166, 83, 151, 168, 168, 35, 27, 252, 211, 180, 7, 189, 232, 51, 111, 88, 185, 215, 102, 24, 68, 160, 68, 175, 3, 199, 212, 43, 237, 184, 123, 143, 237, 44, 31])
+
+let keypair = solanaWeb3.keypair.fromSecretKey(secretKey)
+console.log(keypair)
